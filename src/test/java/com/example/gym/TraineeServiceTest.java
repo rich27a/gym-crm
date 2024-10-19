8d8bf2d7b0daa6a1dcef3be9eaba368f030bfaf7
rich27a <freeazul25@outlook.es>
2024-10-19 11:14:03 -0600

    trainer and trainee service tests addeD

---
+++ src/test/java/com/example/gym/TraineeServiceTest.java
@@ -0,0 +1,54 @@
+package com.example.gym;
+
+import com.example.gym.dao.TraineeDAO;
+import com.example.gym.models.Trainee;
+import com.example.gym.services.TraineeService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TraineeServiceTest {
+
+    @InjectMocks
+    private TraineeService traineeService;
+
+    @Mock
+    private TraineeDAO traineeDAO;
+
+    @Test
+    public void testCreateTrainee(){
+        Mockito.doNothing().when(traineeDAO).save(Mockito.any(Trainee.class));
+
+        Trainee result = traineeService.createTraineeProfile("Juan", "Perez", "25-02-1995", "Calle paz #256");
+
+        Mockito.verify(traineeDAO).save(Mockito.any(Trainee.class));
+
+        assertEquals("Juan", result.getFirstName());
+        assertEquals("Perez", result.getLastName());
+    }
+
+    @Test
+    public void testFindTraineeById() {
+        Trainee trainee = new Trainee();
+        trainee.setId(1);
+        trainee.setFirstName("Juan");
+        trainee.setLastName("Perez");
+
+        Mockito.when(traineeDAO.findById(1)).thenReturn(Optional.of(trainee));
+
+        Optional<Trainee> foundTrainee = traineeService.selectTraineeProfile(1);
+
+        assertTrue(foundTrainee.isPresent());
+        assertEquals("Juan", foundTrainee.get().getFirstName());
+    }
+
+}
---
+++ src/test/java/com/example/gym/TrainerServiceTest.java
@@ -0,0 +1,54 @@
+package com.example.gym;
+
+import com.example.gym.dao.TrainerDAO;
+import com.example.gym.models.Specialization;
+import com.example.gym.models.Trainer;
+import com.example.gym.services.TrainerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class TrainerServiceTest {
+    @InjectMocks
+    private TrainerService trainerService;
+
+    @Mock
+    private TrainerDAO trainerDAO;
+
+    @Test
+    public void testCreateTrainer(){
+        Mockito.doNothing().when(trainerDAO).save(Mockito.any(Trainer.class));
+
+        Trainer result = trainerService.createTrainerProfile("Juan", "Perez", Specialization.CARDIO);
+
+        Mockito.verify(trainerDAO).save(Mockito.any(Trainer.class));
+
+        assertEquals("Juan", result.getFirstName());
+        assertEquals("Perez", result.getLastName());
+    }
+
+    @Test
+    public void testFindTrainerById() {
+        Trainer trainer = new Trainer();
+        trainer.setId(1);
+        trainer.setFirstName("Juan");
+        trainer.setLastName("Perez");
+
+        Mockito.when(trainerDAO.findById(1)).thenReturn(Optional.of(trainer));
+
+        Optional<Trainer> foundTrainer = trainerService.selectTrainerProfile(1);
+
+        assertTrue(foundTrainer.isPresent());
+        assertEquals("Juan", foundTrainer.get().getFirstName());
+    }
+}
