46fd5074e05c98a2ea24e8b4577db5a453e4903a
rich27a <freeazul25@outlook.es>
2024-10-18 23:28:45 -0600

    profile utils added

---
+++ src/main/java/com/example/gym/utils/Profile.java
@@ -0,0 +1,31 @@
+package com.example.gym.utils;
+
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Profile {
+    private static final Map<String, Integer> usernameMap = new HashMap<>();
+    private static final SecureRandom random = new SecureRandom();
+    private static final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateUsername(String firstName, String lastName){
+        String username = firstName + "." + lastName;
+        if (usernameMap.containsKey(username)) {
+            int count = usernameMap.get(username) + 1;
+            usernameMap.put(username, count);
+            return username + count;
+        } else {
+            usernameMap.put(username, 1);
+            return username;
+        }
+    }
+
+    public static String generateRandomPassword() {
+        StringBuilder password = new StringBuilder(10);
+        for (int i = 0; i < 10; i++) {
+            password.append(CHARS.charAt(random.nextInt(CHARS.length())));
+        }
+        return password.toString();
+    }
+}
