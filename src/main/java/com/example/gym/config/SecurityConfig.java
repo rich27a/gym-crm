70df2acad8fda4189df49cc3663822fd23b2d7cd
rich27a <freeazul25@outlook.es>
2024-11-06 19:30:16 -0600

    spring basic auth

--- src/main/java/com/example/gym/config/SecurityConfig.java
+++ src/main/java/com/example/gym/config/SecurityConfig.java
@@ -1,12 +1,56 @@
-package com.example.gym.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-public class SecurityConfig {
-
-}
+//package com.example.gym.config;
+//
+//import com.example.gym.services.CustomUserDetailsService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//public class SecurityConfig {
+//    private final CustomUserDetailsService customUserDetailsService;
+//
+//    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+//        this.customUserDetailsService = customUserDetailsService;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity
+//                .csrf(csrf -> csrf.disable())
+//                .httpBasic(Customizer.withDefaults())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(http -> {
+//                    http.requestMatchers( "/login").permitAll();
+//                    http.requestMatchers( "/api/trainees").permitAll();
+//
+////                    http.requestMatchers(HttpMethod.POST, "/api/trainees").permitAll();
+////                    http.requestMatchers(HttpMethod.POST, "/api/trainers").permitAll();
+//                    http.anyRequest().authenticated();
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+//
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(customUserDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+//}
--- src/main/java/com/example/gym/services/CustomUserDetailsService.java
+++ src/main/java/com/example/gym/services/CustomUserDetailsService.java
@@ -1,26 +1,28 @@
 package com.example.gym.services;
 
-import com.example.gym.models.CustomUserDetails;
-import com.example.gym.repositories.TraineeRepository;
-import com.example.gym.repositories.TrainerRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import com.example.gym.models.CustomUserDetails;
+//import com.example.gym.repositories.TraineeRepository;
+//import com.example.gym.repositories.TrainerRepository;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
 
-public class CustomUserDetailsService implements UserDetailsService {
-    private final TrainerRepository trainerRepository;
-    private final TraineeRepository traineeRepository;
-
-    public CustomUserDetailsService(TrainerRepository trainerRepository, TraineeRepository traineeRepository) {
-        this.trainerRepository = trainerRepository;
-        this.traineeRepository = traineeRepository;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return traineeRepository.findByUsername(username)
-                .map(CustomUserDetails::new)
-                .or(() -> trainerRepository.findByUsername(username).map(CustomUserDetails::new))
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    }
-}
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//    private final TrainerRepository trainerRepository;
+//    private final TraineeRepository traineeRepository;
+//
+//    public CustomUserDetailsService(TrainerRepository trainerRepository, TraineeRepository traineeRepository) {
+//        this.trainerRepository = trainerRepository;
+//        this.traineeRepository = traineeRepository;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        return traineeRepository.findByUsername(username)
+//                .map(CustomUserDetails::new)
+//                .or(() -> trainerRepository.findByUsername(username).map(CustomUserDetails::new))
+//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+//    }
+//}
