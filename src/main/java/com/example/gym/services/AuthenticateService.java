362ea51f65f409da3d71b9893c84e22b15a0cc8b
rich27a <freeazul25@outlook.es>
2024-11-28 10:34:16 -0600

    adding encription to changePassword method

--- src/main/java/com/example/gym/services/AuthenticateService.java
+++ src/main/java/com/example/gym/services/AuthenticateService.java
@@ -3,6 +3,7 @@ package com.example.gym.services;
 import com.example.gym.dtos.LoginPasswordDto;
 import com.example.gym.dtos.PasswordChangeDto;
 import com.example.gym.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,11 +23,18 @@ public class AuthenticateService {
                 .orElse(false);
     }
 
+    @Transactional
     public boolean changePassword(PasswordChangeDto passwordChangeDto){
-        LoginPasswordDto loginPasswordDto = new LoginPasswordDto(passwordChangeDto.getUsername(), passwordChangeDto.getOldPassword());
-        if(authenticate(loginPasswordDto)){
-
-        }
-        return false;
+        return userRepository.findByUsername(passwordChangeDto.getUsername())
+                .map(user -> {
+                    if (passwordEncoder.matches(passwordChangeDto.getOldPassword(), user.getPassword())){
+                        user.setPassword(passwordEncoder.encode(passwordChangeDto.getNewPassword()));
+                        userRepository.save(user);
+                        return true;
+                    }
+                    return false;
+                }
+                )
+                .orElse(false);
     }
 }
