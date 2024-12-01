757e2a023fdadeee316ed1a992f0c2cb1780bb16
rich27a <freeazul25@outlook.es>
2024-11-30 18:15:23 -0600

    adding mapping from trainer to trainerInfo

--- src/main/java/com/example/gym/mappers/TrainerMapper.java
+++ src/main/java/com/example/gym/mappers/TrainerMapper.java
@@ -1,5 +1,6 @@
 package com.example.gym.mappers;
 
+import com.example.gym.dtos.TrainerInfoResponseDTO;
 import com.example.gym.dtos.TrainerProfileResponse;
 import com.example.gym.dtos.TrainerProfileUpdateResponseDTO;
 import com.example.gym.models.Trainer;
@@ -14,9 +15,17 @@ public interface TrainerMapper {
     @Mapping(source = "trainingType", target = "specialization")
     TrainerProfileResponse toResponse(Trainer trainer);
 
+
+    @Mapping(source = "trainingType", target = "specialization")
+    TrainerInfoResponseDTO toTrainerInfoResponse(Trainer trainer);
+
+
+
     @Mapping(source = "trainees", target = "trainees")
     @Mapping(source = "trainingType", target = "specialization")
     TrainerProfileUpdateResponseDTO toUpdateResponse(Trainer trainer);
+
+
 
     default String mapSpecialization(TrainingType specialization) {
         return specialization != null ? specialization.getTrainingType().name() : "UNKNOWN";
