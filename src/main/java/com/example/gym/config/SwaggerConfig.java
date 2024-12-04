5da2961e95e2c4d1274fb617e10e2bd90d98515b
rich27a <freeazul25@outlook.es>
2024-12-04 13:49:55 -0600

    configuring swagger with annotations

--- src/main/java/com/example/gym/config/SwaggerConfig.java
+++ src/main/java/com/example/gym/config/SwaggerConfig.java
@@ -1,18 +1,33 @@
 package com.example.gym.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
-@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API GYM CRM",
+                description = "Our api provides a CRM GYM",
+                version = "1.0.0"
+        ),
+        security = @SecurityRequirement(
+                name = "basicAuth"
+        )
+
+)
+@SecurityScheme(
+        name = "basicAuth",
+        description = "Username and password for my api",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic",
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER
+)
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("API Documentation")
-                        .version("1.0.0")
-                        .description("API Documentation for your project"));
-    }
+
 }
