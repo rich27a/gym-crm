81749d5cb162a7b28a9da9e28a0017687beb9dae
rich27a <freeazul25@outlook.es>
2024-12-03 19:44:37 -0600

    dtos for trainee registration added

---
+++ src/main/java/com/example/gym/dtos/TraineeRegistrationRequestDTO.java
@@ -0,0 +1,49 @@
+package com.example.gym.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public class TraineeRegistrationRequestDTO {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    private String address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
