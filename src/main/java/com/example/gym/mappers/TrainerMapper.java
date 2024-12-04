625f6fbc69d92ed7036983f0db2413c0d58338a1
rich27a <freeazul25@outlook.es>
2024-12-03 19:06:17 -0600

    method mappers for registration trainer added

--- src/main/java/com/example/gym/mappers/TrainerMapper.java
+++ src/main/java/com/example/gym/mappers/TrainerMapper.java
@@ -1,8 +1,6 @@
 package com.example.gym.mappers;
 
-import com.example.gym.dtos.TrainerInfoResponseDTO;
-import com.example.gym.dtos.TrainerProfileResponse;
-import com.example.gym.dtos.TrainerProfileUpdateResponseDTO;
+import com.example.gym.dtos.*;
 import com.example.gym.models.Trainer;
 import com.example.gym.models.TrainingType;
 import org.mapstruct.Mapper;
@@ -19,15 +17,26 @@ public interface TrainerMapper {
     @Mapping(source = "trainingType", target = "specialization")
     TrainerInfoResponseDTO toTrainerInfoResponse(Trainer trainer);
 
-
-
     @Mapping(source = "trainees", target = "trainees")
     @Mapping(source = "trainingType", target = "specialization")
     TrainerProfileUpdateResponseDTO toUpdateResponse(Trainer trainer);
 
 
+    TrainerRegistrationResponseDTO toRegistrationResponse(Trainer trainer);
+
+    @Mapping(target = "username", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(source = "specialization", target = "trainingType")
+    Trainer toTrainerFromRequest(TrainerRegistrationRequestDTO trainerRequest);
+
+
 
     default String mapSpecialization(TrainingType specialization) {
         return specialization != null ? specialization.getTrainingType().name() : "UNKNOWN";
+    }
+
+    default TrainingType mapSpecialization(String specialization) {
+
+        return TrainingType.fromString(specialization);
     }
 }
