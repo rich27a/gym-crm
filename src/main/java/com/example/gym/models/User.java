ab592866655ff82fbcdf489ee5779c65acaedbec
rich27a <freeazul25@outlook.es>
2024-12-05 12:25:13 -0600

    username field unique = true

--- src/main/java/com/example/gym/models/User.java
+++ src/main/java/com/example/gym/models/User.java
@@ -18,7 +18,7 @@ public class User {
     @Column(nullable = false)
     @NotBlank
     private String lastName;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false)
     @JsonIgnore
