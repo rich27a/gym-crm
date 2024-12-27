55f7f7260c6cb97dd0d4b13de14d5768d25c2d46
rich27a <freeazul25@outlook.es>
2024-12-27 11:43:35 -0600

    jwt filter added to securityFilterChain

--- src/main/java/com/example/gym/config/SecurityConfig.java
+++ src/main/java/com/example/gym/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.gym.config;
 
+import com.example.gym.filters.JwtRequestFilter;
 import com.example.gym.services.CustomUserDetailsService;
+import com.example.gym.utils.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,16 +15,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
 
-
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, JwtUtil jwtUtil) {
         this.customUserDetailsService = customUserDetailsService;
+        this.jwtUtil = jwtUtil;
     }
 
     @Bean
@@ -34,12 +38,13 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(http -> {
                     http.requestMatchers( "/login").permitAll();
-                    http.requestMatchers( HttpMethod.GET,"api/auth/login").permitAll();
+                    http.requestMatchers( "api/auth/login").permitAll();
                     http.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
                     http.requestMatchers(HttpMethod.POST, "/api/trainees").permitAll();
                     http.requestMatchers(HttpMethod.POST, "/api/trainers").permitAll();
                     http.anyRequest().authenticated();
                 })
+                .addFilterBefore(new JwtRequestFilter(jwtUtil, customUserDetailsService), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
