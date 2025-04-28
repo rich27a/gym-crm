01608772dc7177c9c565e022b76aa0f10b7e9e0c
rich27a <freeazul25@outlook.es>
2025-04-28 16:49:29 -0600

    workloadsevice client added

--- src/main/java/com/example/gym/client/WorkloadServiceClient.java
+++ src/main/java/com/example/gym/client/WorkloadServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "workload-service")
+@FeignClient(name = "workload-service", url = "localhost:8090")
 public interface WorkloadServiceClient {
 
     @PostMapping("/api/workload")
