390972d2f580dae59132a58f685eeba38c82d978
rich27a <freeazul25@outlook.es>
2025-05-05 11:22:41 -0600

    constants for jms added

---
+++ src/main/java/com/example/gym/constants/JmsConstants.java
@@ -0,0 +1,7 @@
+package com.example.gym.constants;
+
+public class JmsConstants {
+    public static final String WORKLOAD_QUEUE = "gym.workload.queue";
+    public static final String WORKLOAD_DLQ = "gym.workload.dlq";
+    public static final String TRANSACTION_ID_PROPERTY = "X-Transaction-ID";
+}
